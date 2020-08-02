2fb0f3cb86f00112c4d3bde2c423509d9520fb54
Santivi <Santivi@host.docker.internal>
2020-08-02 17:39:08 -0500

    Inicio de proyecto Kleverness

---
+++ Kleverness/src/main/java/com/visa/proyectos/KlevernessApplication.java
@@ -0,0 +1,13 @@
+package com.visa.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlevernessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlevernessApplication.class, args);
+	}
+
+}
---
+++ Kleverness/src/test/java/com/visa/proyectos/KlevernessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa.proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlevernessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
